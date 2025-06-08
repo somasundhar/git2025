79886183c24b39d923afc6b3dbcf97908b27522d
somasundhar <somasundharmynam45@gmail.com>
2025-06-09 01:38:42 +0530

    added h2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
      <h1>Hi SomaSundhar Welcome to GitHub account</h1>
     <h1> hello world </h1>
     <h1> i have created git hub account</h1>
+    <h2>present i am studing mern full stack developer course</h2>
     </div>
   );
 }
